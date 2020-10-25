2e3695b55eea4b90612d546374f4ca5cd7b1f97a
Maftuchati <chaachaati@gmail.com>
2020-10-25 20:19:46 +0700

    first commit

---
+++ Folder 1/pustaka_Maftuchati/application/controllers/perkenalan.php
@@ -0,0 +1,10 @@
+<?php
+class perkenalan extends CI_Controller{
+    public function index() {
+        echo "<h1> perkenalan</h1>";
+        echo "Maftuchati<br>
+                saya tinggal di BREBES<br>
+                saya suka Memasak";
+    }
+
+}
---
+++ Folder 2/PRAKTIKUM 4/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama : Maftuchati</li>
+        
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Gandasuli</li> </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+    <li>Brebes</li></ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Senam</li>
+    </ul>
+</section>
